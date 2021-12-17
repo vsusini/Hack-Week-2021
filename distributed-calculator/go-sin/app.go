6f0ce0fe3fbb03d7afa4a1080cfa995d82912ca0
Vincenzo <vsusini@telesat.com>
2021-12-17 13:59:10 +0000

    cos and sin

---
+++ distributed-calculator/go-sin/app.go
@@ -0,0 +1,38 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package main
+
+import (
+	"encoding/json"
+    "log"
+	"net/http"
+	"fmt"
+	"math"
+	
+	"github.com/gorilla/mux"
+)
+
+type Operands struct {
+    OperandOne float64 `json:"operandOne,string"`
+    OperandTwo float64 `json:"operandTwo,string"`
+}
+
+func add(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	var operands Operands
+	json.NewDecoder(r.Body).Decode(&operands)
+	// fmt.Println(fmt.Sprintf("%s%f", "Sin ", operands.OperandOne))
+	fmt.Println(fmt.Sprintf("%s%f", "Sin ", operands.OperandTwo))
+	json.NewEncoder(w).Encode(math.Round(math.Sin(operands.OperandTwo)*100)/100)
+}
+ 
+func main() {
+	router := mux.NewRouter()
+	
+	router.HandleFunc("/sin", add).Methods("POST", "OPTIONS")
+	log.Fatal(http.ListenAndServe(":6002", router))
+}
